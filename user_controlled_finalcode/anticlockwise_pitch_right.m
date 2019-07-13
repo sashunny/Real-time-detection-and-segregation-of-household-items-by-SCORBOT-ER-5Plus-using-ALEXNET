4632b2281de4e2d1d235b752fdc2345759c05566
sashwata banerjee <sashwata21@gmail.com>
2019-07-13 22:50:06 +0530

    Update anticlockwise_pitch_right.m

--- user_controlled_finalcode/anticlockwise_pitch_right.m
+++ user_controlled_finalcode/anticlockwise_pitch_right.m
@@ -1,7 +1,7 @@
 function anticlockwise_pitch_right(a)        
 
   writePWMDutyCycle(a, 'D10', 0);
-  writePWMDutyCycle(a, 'D11', 1);
+  writePWMDutyCycle(a, 'D11', 1);               %change the duty cycle to change the speed(0 to 1)
   writePWMDutyCycle(a,'D3', 0);
 
 end
