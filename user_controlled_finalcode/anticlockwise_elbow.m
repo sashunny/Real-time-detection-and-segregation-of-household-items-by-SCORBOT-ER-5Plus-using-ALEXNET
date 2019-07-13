a31c154fc5cd6a8aaccec16881e87a947db765e1
sashwata banerjee <sashwata21@gmail.com>
2019-07-13 22:48:26 +0530

    Update anticlockwise_elbow.m

--- user_controlled_finalcode/anticlockwise_elbow.m
+++ user_controlled_finalcode/anticlockwise_elbow.m
@@ -1,11 +1,7 @@
 function anticlockwise_elbow(a)        
 
   writePWMDutyCycle(a,'D44',0);
-  writePWMDutyCycle(a,'D45',0.5);
+  writePWMDutyCycle(a,'D45',0.5);         %change the duty cycle to change the speed(0 to 1)
   writePWMDutyCycle(a,'D2',0);
-
-  %writeDigitalPin(a, 'D44', 0);
-  %writeDigitalPin(a, 'D45', 1);
-  %writeDigitalPin(a,'D2', 0);
 
 end
