bf0b6ec69dfeecd1247e5a77a29f02bd1248e297
sashwata banerjee <sashwata21@gmail.com>
2019-07-13 22:47:07 +0530

    Update anticlockwise_base.m

--- user_controlled_finalcode/anticlockwise_base.m
+++ user_controlled_finalcode/anticlockwise_base.m
@@ -1,6 +1,6 @@
 function anticlockwise_base(a)        
 
-  writePWMDutyCycle(a, 'D9', 0.25);
+  writePWMDutyCycle(a, 'D9', 0.25);    %change the duty cycle to change the speed
   writePWMDutyCycle(a, 'D8', 0);
   writePWMDutyCycle(a,'D5', 0);
 
